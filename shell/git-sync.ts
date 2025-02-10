f64ee9616489aa1290d9b8e009268a4f6184358f
墨七 <meichangliang@outlook.com>
2025-02-10 15:52:41 +0800

    添加脚本

---
+++ shell/git-sync.ts
@@ -0,0 +1,31 @@
+#!bun
+import { $ } from "bun";
+import { myInit } from "./config";
+
+await myInit();
+
+let desc = process.argv[2];
+if (!desc) {
+  desc = "sync-commit-default";
+  console.warn(`git commit: ${desc} \n`);
+} else {
+  console.log(`git commit: ${desc} \n`);
+}
+
+try {
+  await $`git pull`;
+  await $`git add .`;
+  await $`git commit -m "${desc}"`;
+  await $`git push`;
+} catch (error) {
+  if (
+    error.stdout.toString().includes("nothing to commit, working tree clean")
+  ) {
+    process.exit(0);
+  } else {
+    console.error(`git err code: ${error.exitCode}`);
+    throw new Error(error.stdout.toString());
+  }
+}
+
+process.exit(0);
